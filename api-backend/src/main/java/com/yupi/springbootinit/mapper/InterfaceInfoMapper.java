e1411e77634428540994d2c973eb24d5d10596df
hcj <hcj@git.cn>
2024-04-19 17:38:33 +0800

    实现接口的增删改查等基础操作

---
+++ api-backend/src/main/java/com/yupi/springbootinit/mapper/InterfaceInfoMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.springbootinit.mapper;
+
+import com.yupi.springbootinit.model.entity.InterfaceInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 兕神
+* @description 针对表【interface_info(接口信息)】的数据库操作Mapper
+* @createDate 2024-04-19 09:33:46
+* @Entity com.yupi.springbootinit.model.entity.InterfaceInfo
+*/
+public interface InterfaceInfoMapper extends BaseMapper<InterfaceInfo> {
+
+}
+
+
+
+
