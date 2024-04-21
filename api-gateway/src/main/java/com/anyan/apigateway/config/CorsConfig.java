beeb6dfffdc914c6f1915142b0b5f871bf05a982
hcj <hcj@git.cn>
2024-04-21 21:07:09 +0800

    跨域配置(前端到网关)

--- api-gateway/src/main/java/com/anyan/apigateway/config/CorsConfig.java
+++ api-gateway/src/main/java/com/anyan/apigateway/config/CorsConfig.java
@@ -6,15 +6,17 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
-/**全局跨域配置
+/**
+ * 全局跨域配置
+ *
  * @author 兕神
  * DateTime: 2024/4/21
  */
 @Configuration
-public class CorsConfig{
+public class CorsConfig {
 
     @Bean
-    public CorsWebFilter corsWebFilter(){
+    public CorsWebFilter corsWebFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         //1、配置跨域
@@ -24,7 +26,7 @@ public class CorsConfig{
         corsConfiguration.setAllowCredentials(true);
 
 
-        source.registerCorsConfiguration("/**",corsConfiguration);
+        source.registerCorsConfiguration("/**", corsConfiguration);
         return new CorsWebFilter(source);
     }
 }
