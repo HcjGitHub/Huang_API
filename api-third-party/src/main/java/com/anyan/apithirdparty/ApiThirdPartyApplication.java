8f98b309a1d7147309a32a007c07e0d255a0f89c
hcj <hcj@git.cn>
2024-05-01 15:07:33 +0800

    将Dubbo缓存加入gitignore

--- api-third-party/src/main/java/com/anyan/apithirdparty/ApiThirdPartyApplication.java
+++ api-third-party/src/main/java/com/anyan/apithirdparty/ApiThirdPartyApplication.java
@@ -11,6 +11,11 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class ApiThirdPartyApplication {
 
     public static void main(String[] args) {
+        //将dubbo缓存的绝对目录改成相对目录，避免后续项目上线出现问题 已实现
+        String rootPath = System.getProperty("user.dir");
+        String subDirectory = "/api-third-party/thirdPartyDubboCache";
+        String fullPath = rootPath + "/" + subDirectory;
+        System.setProperty("user.home", fullPath);
         SpringApplication.run(ApiThirdPartyApplication.class, args);
     }
 
